384ffe3f647aae648a20aaf04621f4f309c24178
samuel hostia <samuelhostia63@gmail.com>
2026-09-08 16:19:59 -0500

    feat(ThemeContext): Se añadió el theme light-dark

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App";
 
+import {
+  ThemeProvider,
+} from "./context/ThemeContext";
+
 import "./styles/global.css";
 
-const queryClient = new QueryClient();
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-		</QueryClientProvider>
-	</React.StrictMode>,
+ReactDOM.createRoot(
+  document.getElementById("root")
+).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
